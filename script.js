6650bcab519c9e13d611cee105da57cec49c31bd
Cody Yuill <codyyuill@hotmail.com>
2020-12-08 13:54:09 -0500

    made a test call to get 7 day forecast of ottawa logged to conolse

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 $("h1").text("weather updated");
+
+var owKey = "82db3485b1bdbce46c2ce5d7d2f3ae57";
+$.ajax({
+    //ottawa to test
+    url: `https://api.openweathermap.org/data/2.5/onecall?lat=45.421532&lon=-75.697189&appid=${owKey}`,
+    method: "GET"
+}).then(function(data){
+    console.log(data);
+});
